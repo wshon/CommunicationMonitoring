84592463d8f1294fdaa408e95ce2862011ca7f56
森 王 <xxwsxx@LIVE.COM>
2017-08-05 02:14:35 +0800

    增加虚拟串口功能

---
+++ VirtualSerialMonitor/MainForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AxVSPortLib;
+using VSPortLib;
+
+namespace VirtualSerialMonitor
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm(string title, string userSerialPort)
+        {
+            InitializeComponent();
+            this.Text = title;
+            CreatPort(userSerialPort);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DeletaPort();
+        }
+
+
+        public enum Error
+        {
+            NoError,
+            Error,
+            IsNotCreat,
+            PortCreatError,
+            PortDeleteError,
+
+            IsNotPair,
+            PairCreatError,
+            PortOneIsCreated,
+            PortTwoIsCreated,
+        };
+
+        private VSPortAx axVSPortAx = new VSPortAx();
+        private string port;
+
+        public Error CreatPort(string strPort1)
+        {
+            port = strPort1;
+            if (axVSPortAx.CreatePort(strPort1) || axVSPortAx.Attach(strPort1))
+            {
+                axVSPortAx.SetBreak(true);
+                axVSPortAx.SetStrictBaudrate(true);
+                axVSPortAx.SetWiring(0x20, 0x10, 0, 0);
+                return Error.NoError;
+            }
+            else
+            {
+                return Error.Error;
+            }
+        }
+
+        public Error DeletaPort()
+        {
+            if (axVSPortAx.IsCreated)
+            {
+                if (axVSPortAx.Delete())
+                {
+                    return Error.NoError;
+                }
+                else
+                {
+                    return Error.Error;
+                }
+            }
+            else
+            {
+                return Error.IsNotCreat;
+            }
+        }
+
+        public Error ResetBus()
+        {
+            if (!axVSPortAx.ResetBus())
+            {
+                return Error.NoError;
+            }
+            else
+            {
+                return Error.Error;
+            }
+        }
+    }
+}
---
+++ VirtualSerialMonitor/StartForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VirtualSerialMonitor
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm frmSerialFrom = new MainForm(textBox1.Text, textBox2.Text);
+            frmSerialFrom.MdiParent = this.ParentForm.Owner;
+            frmSerialFrom.Show();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "regsvr32";
+            p.StartInfo.Verb = "runas";
+            p.StartInfo.Arguments = System.Windows.Forms.Application.StartupPath + "\\VSPort.dll";
+            p.Start();
+            p.WaitForExit();
+            p.StartInfo.Arguments = System.Windows.Forms.Application.StartupPath + "\\VSPort64.dll";
+            p.Start();
+            p.WaitForExit();
+            p.Close();
+            p.Dispose();
+        }
+    }
+}
